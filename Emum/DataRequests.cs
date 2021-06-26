342483a2f8f7beb54f00bc830a02f45de1da27c1
Jaap <jaap@mrproper.dev>
2021-06-26 12:44:12 +0200

    DataRequests Enum

---
+++ Emum/DataRequests.cs
@@ -0,0 +1,12 @@
+﻿namespace Simvars.Emum
+{
+    public enum DataRequests
+    {
+        SIM_STATE,
+        AIRCRAFT_POSITION,
+        AI_DESPAWN,
+        AI_RELEASE,
+
+        AI_SPAWN = 10000, // 10000 to 19999
+    }
+}
