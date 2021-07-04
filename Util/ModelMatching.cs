351ecd0db4f9b564bd5fd26793d6f4bf610356cc
Jaap <jaap@mrproper.dev>
2021-07-04 17:28:29 +0200

    Improve livery matching

--- Util/ModelMatching.cs
+++ Util/ModelMatching.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Simvars.Util
 {
@@ -11,30 +12,46 @@ namespace Simvars.Util
         {
             List<string> installedLiveries = liveries ?? AddonScanner.ScanAddons();
 
-            Console.WriteLine("Model matching:" + model + " with airline: " + airline);
+            Console.WriteLine($"Model matching: {model} with airline: {airline}");
             JObject models = JObject.Parse(File.ReadAllText(@".\Config\ModelMatching.json"));
-            if (models.GetValue(model) == null) Console.WriteLine("Failed to model match: " + model);
+            if (models.GetValue(model) == null) Console.WriteLine($"Failed to model match: {model}");
             string matchedModel = (string)models.GetValue(model) ?? (string)models.GetValue("Default Aircraft") ?? "Airbus A320 Neo";
 
-            if (models.GetValue(matchedModel + " " + airline) != null)
+            if (TryFindAircraft(models, installedLiveries, $"{matchedModel} {airline}") != null)
             {
-                matchedModel = (string) models.GetValue(matchedModel + " " + airline);
+                matchedModel = TryFindAircraft(models, installedLiveries, $"{matchedModel} {airline}");
             }
-            else if (installedLiveries.Contains(matchedModel + " " + airline + " AI"))
+            else if (airline.Contains("(") && TryFindAircraft(models, installedLiveries, $"{matchedModel} {airline.Split('(')[0].Trim()}") != null)
             {
-                matchedModel = matchedModel + " " + airline + " AI";
-            }
-            else if (installedLiveries.Contains(matchedModel + " " + airline))
-            {
-                matchedModel = matchedModel + " " + airline;
+                matchedModel = TryFindAircraft(models, installedLiveries, $"{matchedModel} {airline.Split('(')[0].Trim()}");
             }
             else
             {
-                if (models.GetValue(matchedModel + " Default") == null) Console.WriteLine("Failed to model match: " + matchedModel + " Default");
-                matchedModel = (string)models.GetValue(matchedModel + " Default") ?? "Airbus A320 Neo Asobo";
+                if (models.GetValue(matchedModel + " Default") == null) Console.WriteLine($"Failed to model match: {matchedModel} Default");
+                matchedModel = (string)models.GetValue($"{matchedModel} Default") ?? "Airbus A320 Neo Asobo";
             }
-            Console.WriteLine("Model matched " + model + " " + airline + " with " + matchedModel);
+            Console.WriteLine($"Model matched {model} {airline} with: {matchedModel}");
             return matchedModel;
+        }
+
+        private static string TryFindAircraft(JObject models, List<string> installedLiveries, string fullName)
+        {
+            string foundAircraft = null;
+
+            if (models.GetValue(fullName) != null)
+            {
+                foundAircraft = (string)models.GetValue(fullName);
+            }
+            else if (installedLiveries.Contains($"{fullName} AI"))
+            {
+                foundAircraft = $"{fullName} AI";
+            }
+            else if (installedLiveries.Contains(fullName))
+            {
+                foundAircraft = fullName;
+            }
+
+            return foundAircraft;
         }
     }
 }
